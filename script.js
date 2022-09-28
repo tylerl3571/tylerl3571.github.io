8a62969aef7f8cf6f299c3110011432b76941b4b
tylerl3571 <114585709+tylerl3571@users.noreply.github.com>
2022-09-28 08:36:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Tyler Lee"; // update the part BETWEEN the "quotes"
+let userName = "tylerl3571"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
